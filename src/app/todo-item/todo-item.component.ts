7458af45604b6f76d7fd00249c6854933d0cc6b9
devfdom@gmail.com <devfdom@gmail.com>
2022-05-27 00:48:38 +0200

    deleteBtn

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,20 +1,34 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import tippy from 'tippy.js';
 import { Todo } from '../shared/todo.model';
+
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss']
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent implements OnInit, AfterViewInit {
 
   @Input() todo: Todo
   @Output() todoClicked: EventEmitter<void> = new EventEmitter()
   @Output() editClicked: EventEmitter<void> = new EventEmitter()
+  @Output() deleteClicked: EventEmitter<void> = new EventEmitter()
+
+  @ViewChild('editBtn') editBtnElRef: ElementRef<HTMLElement>
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("This is the edit btn element")
+    console.log(this.editBtnElRef.nativeElement)
+    tippy(this.editBtnElRef.nativeElement, {
+      content: 'Test Tooltip!'
+    })
+  }
+
+  ngAfterViewInit(): void {
+    
   }
 
   onTodoClicked() {
@@ -23,6 +37,9 @@ export class TodoItemComponent implements OnInit {
   }
   onEditClicked() {
     this.editClicked.emit()
+  }
+  onDeleteClicked() {
+    this.deleteClicked.emit()
   }
 
 }
