49815184829a772a5d1b9e5f404f169182677edd
devfdom@gmail.com <devfdom@gmail.com>
2022-05-27 02:30:34 +0200

    final

--- src/app/shared/tool-tip-singleton.directive.ts
+++ src/app/shared/tool-tip-singleton.directive.ts
@@ -1,17 +1,33 @@
-import { ToolTipDirective } from './tool-tip.directive';
-import { ContentChildren, Directive, QueryList, AfterViewInit } from '@angular/core';
+import { AfterViewInit, ContentChildren, Directive, QueryList } from '@angular/core';
 import { createSingleton } from 'tippy.js';
+import { ToolTipDirective } from './tool-tip.directive';
 
 @Directive({
   selector: '[appToolTipSingleton]'
 })
-export class ToolTipSingletonDirective implements AfterViewInit {
+export class ToolTipSingletonDirective implements AfterViewInit{
 
-  @ContentChildren(ToolTipDirective, { descendants: true})
-  elementsWithTooltips: QueryList<ToolTipDirective>
+  @ContentChildren(ToolTipDirective,{descendants:true}) 
+  elementsWithToolTips:QueryList<ToolTipDirective>
+
+  singletonInstance: any;
   constructor() { }
 
-  ngAfterViewInit(): void {
-    
+  ngAfterViewInit(){
+    this.singletonInstance = createSingleton(this.getTippyInstances(), {
+      delay: [200, 0],
+      moveTransition: 'transform 0.2s ease-out'
+    })
+    this.elementsWithToolTips.changes.subscribe(() => {
+      this.singletonInstance.setInstances(this.getTippyInstances())
+    })
   }
+
+  getTippyInstances(){
+    return this.elementsWithToolTips
+      .toArray()
+      .map(t => t.tippyInstance)
+
+  }
+
 }
